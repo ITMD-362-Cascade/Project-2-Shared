97b47a635c33109493de2149089a02e63986c981
Piotr Rajchel <59983531+swiftzircon@users.noreply.github.com>
2020-11-15 18:40:10 -0600

    Shortened text further to fit better

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@ function animationFunction() {
     news.textContent = ("Austria announces strict lockdown as virus cases soar") ;
 	newsNumber++;
   } else if (newsNumber == 1){
-    news.textContent = ("New law on 'political Islam' opens door for crackdown on Muslims");	
+    news.textContent = ("New law on 'political Islam' relating to crackdown on Muslims");	
 	newsNumber++;
   } else if (newsNumber == 2){
-    news.textContent = ("New Hydrogen train is set to travel on challenging alpine routes") ;
+    news.textContent = ("New Hydrogen train is set to travel on alpine routes") ;
 	newsNumber = 0;
   };
 };
