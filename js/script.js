0b011ee9d7ec19cfbf52748c499eed7d4b52606d
ymtmb <60162839+ymtmb@users.noreply.github.com>
2020-11-19 17:56:26 -0600

    Grammar issue
    added an "and a name" instead of "and name

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ var email = document.getElementById("eAddress");
 
 form.onsubmit = function() {
   if (email.value == "") {
-    alert("Signup requires that you at least enter an Email Address and name.");
+    alert("Signup requires that you at least enter an Email Address and a name.");
 	return false;
   }
 }
