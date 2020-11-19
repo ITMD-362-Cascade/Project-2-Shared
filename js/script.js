cbcc1100d87b9ed5cf8edbdfa32aecf87c812da3
Piotr Rajchel <59983531+swiftzircon@users.noreply.github.com>
2020-11-19 02:30:34 -0600

    Added javascript for field checking on form
    The form on the Conferences page will now verify that the user has at least entered their email to continue.

--- js/script.js
+++ js/script.js
@@ -16,4 +16,14 @@ function animationFunction() {
   };
 };
 
+var form = document.getElementById("signup_form");
+var email = document.getElementById("eAddress");
+
+form.onsubmit = function() {
+  if (email.value == "") {
+    alert("Signup requires that you at least enter an Email Address.");
+	return false;
+  }
+}
+
 
