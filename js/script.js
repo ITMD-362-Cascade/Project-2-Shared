95cdc77dee089de0e7b5bad7d70aed18fe4f0d2b
Piotr Rajchel <59983531+swiftzircon@users.noreply.github.com>
2020-11-20 00:54:04 -0600

    Modified script
    Condensed javascript, didn't need vars, and there was a missing parenthesis in the if statement.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
-var form = document.getElementById("signup_form");
-var email = document.getElementById("eAddress");
-
-form.onsubmit = function() {
-  if (email.value == "") {
+document.getElementById("signup_form").onsubmit = function() {
+  if (document.getElementById("eAddress").value == "") {
     alert("Signup requires that you at least enter an Email Address.");
 	return false;
   }
