9c01bd7ab8270d2edb650fecb86d738a5602cc8e
ymtmb <60162839+ymtmb@users.noreply.github.com>
2020-11-20 00:01:57 -0600

    Removed uneed js
    Since it wasn't working or we couldn't figure it to work, we decided to just remove it.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-var news = document.getElementById("news_text");
-var newsNumber = 0;
-
-news.addEventListener('animationiteration', animationFunction);
- 
-function animationFunction() {
-  if (newsNumber == 0){
-    news.textContent = ("Austria announces strict lockdown as virus cases soar") ;
-	newsNumber++;
-  } else if (newsNumber == 1){
-    news.textContent = ("New law on 'political Islam' relating to crackdown on Muslims");	
-	newsNumber++;
-  } else if (newsNumber == 2){
-    news.textContent = ("New Hydrogen train is set to travel on alpine routes") ;
-	newsNumber = 0;
-  };
-};
 
 var form = document.getElementById("signup_form");
 var email = document.getElementById("eAddress");
