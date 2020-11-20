a2ab2ec755744b99ed25d5af83e21f16650ab8d0
ymtmb <60162839+ymtmb@users.noreply.github.com>
2020-11-20 00:13:53 -0600

    Removed the news css
    The javascript animation for the news ticker wasn't displaying properly for all users, and is being removed and we'll add a different element in its place

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
+<<<<<<< Updated upstream
 var form = document.getElementById("signup_form");
 var email = document.getElementById("eAddress");
 
@@ -8,5 +9,21 @@ form.onsubmit = function() {
 	return false;
   }
 }
+=======
+news.addEventListener('animationiteration', animationFunction);
+ 
+function animationFunction() {
+  if (newsNumber == 0){
+    news.textContent = ("Austria announces strict lockdown as virus cases soar") ;
+	newsNumber++;
+  } else if (newsNumber == 1){
+    news.textContent = ("New law on 'political Islam' relating to crackdown on Muslims");	
+	newsNumber++;
+  } else if (newsNumber == 2){
+    news.textContent = ("New Hydrogen train is set to travel on alpine routes") ;
+	newsNumber = 0;
+  };
+};
+>>>>>>> Stashed changes
 
 
