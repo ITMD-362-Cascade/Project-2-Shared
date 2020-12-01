af0d349614d7bb698363b2cde93950107101dee4
ymtmb <60162839+ymtmb@users.noreply.github.com>
2020-12-01 13:24:16 -0600

    added the basic java format
    I added the basic java format to start with. I will then edit this when I add everything

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ document.getElementById("signup_form").onsubmit = function() {
 	return false;
   }
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
