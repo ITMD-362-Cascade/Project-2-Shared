6540bd878cbc9014237855e48b6572e1070c6892
ymtmb <60162839+ymtmb@users.noreply.github.com>
2020-12-01 13:50:08 -0600

    Added "+" and "-" signs to the blocks for accordions

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ document.getElementById("signup_form").onsubmit = function() {
   }
 }
 
+
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -12,10 +13,10 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
     } else {
-      panel.style.display = "block";
-    }
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
   });
 }
